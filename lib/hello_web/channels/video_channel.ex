a555fa367683b24b75df9429d3d7bedea54ee378
Alexey Shilin <shilin.alexey@gmail.com>
2021-06-17 00:15:37 +0300

    Chg: channels - send/receive events

--- lib/hello_web/channels/video_channel.ex
+++ lib/hello_web/channels/video_channel.ex
@@ -5,16 +5,26 @@ defmodule HelloWeb.VideoChannel do
   def join("videos:" <> video_id, _params, socket) do
     #{:ok, assign(socket, :video_id, String.to_integer(video_id))}
 
-    :timer.send_interval(5_000, :ping)
+#    :timer.send_interval(5_000, :ping)
     {:ok, socket}
 
   end
 
-	def handle_info(:ping, socket) do
-		count = socket.assigns[:count] || 1
-		push(socket, "ping", %{count: count})
+#	def handle_info(:ping, socket) do
+#		count = socket.assigns[:count] || 1
+#		push(socket, "ping", %{count: count})
+#
+#		{:noreply, assign(socket, :count, count + 1)}
+#	end
 
-		{:noreply, assign(socket, :count, count + 1)}
-	end
+  def handle_in("new_annotation", params, socket) do
+    broadcast!(socket, "new_annotation", %{
+      user: %{username: "anon"},
+      body: params["body"],
+      at: params["at"]
+    })
+
+    {:reply, :ok, socket}
+  end
 
 end
