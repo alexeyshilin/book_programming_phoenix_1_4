251b55858be4bac3a5abc45a4a9263f81882092a
Alexey Shilin <shilin.alexey@gmail.com>
2021-06-26 22:12:34 +0300

    Add: wolfram - caching

--- apps/info_sys/lib/info_sys.ex
+++ apps/info_sys/lib/info_sys.ex
@@ -7,21 +7,53 @@ defmodule InfoSys do
     defstruct score: 0, text: nil, backend: nil
   end
 
+  alias InfoSys.Cache
+
   def compute(query, opts \\ []) do
-    timeout = opts[:timeout] || 10_000 
+    timeout = opts[:timeout] || 10_000
     opts = Keyword.put_new(opts, :limit, 10)
     backends = opts[:backends] || @backends
 
-    backends
+    {uncached_backends, cached_results} = 
+      fetch_cached_results(backends, query, opts) 
+
+    uncached_backends
     |> Enum.map(&async_query(&1, query, opts))
-    |> Task.yield_many(timeout) 
-    |> Enum.map(fn {task, res} -> res || Task.shutdown(task, :brutal_kill) end) 
+    |> Task.yield_many(timeout)
+    |> Enum.map(fn {task, res} -> 
+        res || Task.shutdown(task, :brutal_kill) 
+      end)
     |> Enum.flat_map(fn
       {:ok, results} -> results
       _ -> []
     end)
+    |> write_results_to_cache(query, opts) 
+    |> Kernel.++(cached_results) 
     |> Enum.sort(&(&1.score >= &2.score))
     |> Enum.take(opts[:limit])
+  end
+
+  defp fetch_cached_results(backends, query, opts) do 
+    {uncached_backends, results} =
+      Enum.reduce(
+        backends, 
+        {[], []}, 
+        fn backend, {uncached_backends, acc_results} ->
+          case Cache.fetch({backend.name(), query, opts[:limit]}) do
+            {:ok, results} -> {uncached_backends, [results | acc_results]}
+            :error -> {[backend | uncached_backends], acc_results}
+          end
+        end)
+
+    {uncached_backends, List.flatten(results)}
+  end
+
+  defp write_results_to_cache(results, query, opts) do 
+    Enum.map(results, fn %Result{backend: backend} = result ->
+      :ok = Cache.put({backend.name(), query, opts[:limit]}, result)
+
+      result
+    end)
   end
 
   defp async_query(backend, query, opts) do 
@@ -33,3 +65,11 @@ end
 
 # MIX_ENV=dev WOLFRAM_APP_ID=test /usr/local/share/libs/elixir-1.11.4/bin/iex -S mix
 # InfoSys.compute("what is the meaning of life?")
+
+# MIX_ENV=dev WOLFRAM_APP_ID=test /usr/local/share/libs/elixir-1.11.4/bin/iex -S mix
+# :timer.tc(InfoSys, :compute, ["how old is the universe?"])
+# :timer.tc(InfoSys, :compute, ["how old is the universe?"])
+# :timer.tc(InfoSys, :compute, ["what is the meaning of life?"])
+# :timer.tc(InfoSys, :compute, ["what is the meaning of life?"])
+# :timer.tc(InfoSys, :compute, ["1 + 1"])
+# :timer.tc(InfoSys, :compute, ["1 + 1"])
